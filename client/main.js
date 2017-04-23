f625a3318e43521bbbcaa5ca1059175ba45ff97a
dyadyaJora <ja_massa97@mail.ru>
2017-04-23 19:04:27 +0300

    подключил сокеты на клиенте

--- client/main.js
+++ client/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Konva from 'konva'
+import io from 'socket.io-client'
 
-let my = 11;
+let socket = io('http://localhost:8080');
+console.log(socket);
 new Vue({
   el: '#app',
   render: h => h(App)
+});
+
+socket.on('connected', function(data) {
+	console.log('Connected new user:', data);
 })
+
+socket.on('moved', function(data) {
+    console.log('New position:', data);
+});
+
+socket.on('disconnected', function(data) {
+	console.log('Disconnected:', data);
+});
+
+socket.emit('move', { x: 1, y: 1 });
