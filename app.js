23d4aeae17b02acafb52443b3698fd4345dc9e5b
dyadyaJora <ja_massa97@mail.ru>
2017-04-23 14:58:23 +0300

    реструктуризация папок

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let app = require('http').createServer(function (data) {
 });
 let io = require('socket.io')(app);
 let fs = require('fs');
-let WarlockServer = require('./warlock-server');
+let WarlockServer = require('./server/warlock-server');
 const server = new WarlockServer();
 
 app.listen(8080);
