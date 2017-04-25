61f7410a00a7226f5984ac8a1bdf8cb4d6f4a0e0
dyadyaJora <ja_massa97@mail.ru>
2017-04-26 01:06:18 +0300

    try: добавил обработку на W+A и т д :rocket:

--- client/main.js
+++ client/main.js
@@ -79,16 +79,37 @@ socket.on('disconnected', function(data) {
     layer.draw();
 });
 
+let pressed = [];
+
+window.addEventListener('keydown', function(e) {
+    pressed.indexOf(e.code) == -1 && pressed.push(e.code);
+});
+
 window.addEventListener('keypress', function(e) {
-    switch (e.code) {
-        case 'KeyW': socket.emit('move', { x: 0, y: -1}); break;
-        case 'KeyA': socket.emit('move', { x: -1, y: 0}); break;
-        case 'KeyS': socket.emit('move', { x: 0, y: 1}); break;
-        case 'KeyD': socket.emit('move', { x: 1, y: 0}); break;
+    //
+    if (pressed.length && e.code == pressed[pressed.length - 1] ) {
+        if (pressed.length == 1) {
+            switch (e.code) {
+                case 'KeyW': socket.emit('move', { x: 0, y: -1}); break;
+                case 'KeyA': socket.emit('move', { x: -1, y: 0}); break;
+                case 'KeyS': socket.emit('move', { x: 0, y: 1}); break;
+                case 'KeyD': socket.emit('move', { x: 1, y: 0}); break;
+            }
+        } else {
+            switch (pressed.slice(pressed.length - 2).sort().join('+')) {
+                case 'KeyA+KeyW': socket.emit('move', { x: -1, y: -1}); break;
+                case 'KeyA+KeyS': socket.emit('move', { x: -1, y: 1}); break;
+                case 'KeyD+KeyW': socket.emit('move', { x: 1, y: -1}); break;
+                case 'KeyD+KeyS': socket.emit('move', { x: 1, y: 1}); break;
+            }
+        }
     }
     layer.draw();
 });
 
+window.addEventListener('keyup', function(e) {
+    pressed.indexOf(e.code) !== -1 && pressed.splice(pressed.indexOf(e.code), 1);
+});
 stage.on('contentClick', function(e) {
     let data = calcSkillVector(currentLocation, stage.getPointerPosition());
     //если левая кнопка мышки
